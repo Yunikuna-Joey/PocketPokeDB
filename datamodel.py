55188ee407945777020b9ae21e85055f7f7066b0
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-11-06 16:20:33 -0800

    [update]
    => consistent variable name convention
    
    => created new function to help populate the card list

--- datamodel.py
+++ datamodel.py
@@ -8,7 +8,8 @@ Base = declarative_base()
 class Card(Base):
     __tablename__ = 'card'
 
-    nationalDexId = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    cardDexId = Column(Integer)
     name = Column(String(100), nullable=False)
     # foreign key linking to the CollectionSet table 
     collectionSetId = Column(Integer, ForeignKey('collection_set.setId'), nullable=False)
@@ -18,23 +19,23 @@ class Card(Base):
     hitPoints = Column(Integer)
     energyType = Column(String(50))
     holoRating = Column(String(50)) # star1, star2, crown, etc
-    energy_required = Column(String(100))
+    energyRequired = Column(String(100))
 
     # Not all cards have an ability, so these fields can be null, 
     ability = Column(String(100), nullable=True)
-    ability_description = Column(String(100), nullable=True)
+    abilityDescription = Column(String(100), nullable=True)
 
     # every pokemon is guaranteed to have one attack move
-    attack_name = Column(String(100))
-    attack_damage = Column(String(50))
-    attack_description = Column(String(50), nullable=True)
+    attackName = Column(String(100))
+    attackDamage = Column(String(50))
+    attackDescription = Column(String(50), nullable=True)
     
-    attack_name2 = Column(String(100), nullable=True)
-    attack_damage2 = Column(String(50), nullable=True)
-    attack_description2 = Column(String(50), nullable=True)
+    attackName2 = Column(String(100), nullable=True)
+    attackDamage2 = Column(String(50), nullable=True)
+    attackDescription2 = Column(String(50), nullable=True)
     
     weakness = Column(String(100))
-    retreat_cost = Column(String(50))
+    retreatCost = Column(String(50))
 
     # This is for programatically establishing the relationship
     collectionSet = relationship('CollectionSet', back_populates='cards')
@@ -45,6 +46,7 @@ class CollectionSet(Base):
 
     setId = Column(Integer, primary_key=True)
     collectionName = Column(String(100), nullable=False)                    # This will be the name of the booster pack [going to override 'unique=True' for now]
+    collectionId = Column(String(5))
     pokemonCover = Column(String(100))                                      # This will represent the pokemon on the cover of the booster pack [name]
     coverArt = Column(String(255))                                           # This will represent a url containing the image of the booster pack i guess 
 
--- helper.py
+++ helper.py
@@ -11,8 +11,9 @@ def populateCollectionSet(fileName, databaseSession):
         for row in reader: 
             # create a new collection instance
             collection = CollectionSet(
-                setId = row['setId'], 
+                setId = row['setId'],
                 collectionName = row['collectionName'], 
+                collectionId = row['collectionId'], 
                 pokemonCover = row['pokemonCover'], 
                 coverArt = row['coverArt']
             )
@@ -20,4 +21,35 @@ def populateCollectionSet(fileName, databaseSession):
             databaseSession.add(collection)
         
         databaseSession.commit()
+
+def populateCardTable(fileName, databaseSession): 
+    with open(fileName, mode='r', encoding='utf-8') as file: 
+        reader = csv.DictReader(file)
+
+        for row in reader: 
+            card = Card(
+                id = row['id'], 
+                cardDexId = row['cardDexId'], 
+                name = row['name'], 
+                collectionSetId = row['collectionSetId'], 
+                rarity = row['rarity'], 
+                coverArt = row['coverArt'], 
+                hitPoints = row['hitPoints'], 
+                energyType = row['energyType'], 
+                holoRating = row['holoRating'], 
+                energyRequired = row['energyRequired'], 
+                ability = row['ability'], 
+                abilityDescription = row['abilityDescription'], 
+                attackName = row['attackName'], 
+                attackDamage = row['attackDamage'], 
+                attackDescription = row['attackDescription'], 
+                attackName2 = row['attackName2'], 
+                attackDamage2 = row['attackDamage2'], 
+                attackDescription2 = row['attackDescription2'], 
+                weakness = row['weakness'],
+                retreatCost = row['retreatCost']
+            )        
+
+            databaseSession.add(card)
         
+        databaseSession.commit()
