f2131310836f5d1e7170b29cc4f857f2a1796a9f
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-11-15 01:34:37 -0800

    [update]
    => Created new base set for booster packs
    
    => endpoint now queries the base set for booster packs
    
    => styled the card | pack info page

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # helper file imports 
-from datamodel import initializeDatabase, Event, CollectionSet 
-from helper import dropEventTable, populateCollectionSet, populateCardTable, dropCardTable, populateEventTable
+from datamodel import initializeDatabase, Event, CollectionSet, FamilySet 
+from helper import dropCollectionTable, dropEventTable, populateCollectionSet, populateCardTable, dropCardTable, populateEventTable, populateFamilySetTable
 
 # Configure our app values and where the app should be looking for different resources
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -61,10 +61,10 @@ def loadLandingPage():
 def requestAllPackInfo(): 
 
     #* Query the database for all of the booster packs in the db 
-    packList = dbSession.query(CollectionSet).all()
+    packList = dbSession.query(FamilySet).all()
 
     #* Iterate through all of the queried pack information and format into json object 
-    formatPackList = [pack.setFormatJson() for pack in packList]
+    formatPackList = [pack.fSetFormatJson() for pack in packList]
 
     for pack in formatPackList: 
         pack['coverArt'] = url_for('static', filename=f'images/{pack["coverArt"]}')
@@ -76,9 +76,10 @@ if __name__ == '__main__':
     # populateCollectionSet('Sheet1.csv', dbSession)
     # populateCardTable('Sheet2.csv', dbSession)
     # populateEventTable('Sheet3.csv', dbSession)
+    # populateFamilySetTable('Sheet4.csv', dbSession)
 
     # dropAllTable(dbSession)
     # dropEventTable(dbSession)
-    # populateEventTable('Sheet3.csv', dbSession)
+    # populateEventTable('Sheet3.csv', dbSession)    
     
     app.run(port=5500, debug=True)
