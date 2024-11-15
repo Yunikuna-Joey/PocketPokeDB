d50cd63db36ff4d73c5aede5b706e210bd5d5bd4
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-11-14 17:19:06 -0800

    [misc]
    => tentative plan

--- react-app/src/components/CardPackInfo.js
+++ react-app/src/components/CardPackInfo.js
@@ -1,6 +1,21 @@
 export const CardPackInfo = () => { 
     // we will need to load in the pack [3]
 
+    /* 
+        Tentative Plan: 
+        --> Load in only the Parent name for the booster packs (i.e Genetic Apex, Promotional Pack A)
+        
+        Option #1
+            --> The booster pack tile will then lead to the different faces (if any) 
+            --> Then another click into the face pack 
+            --> Then display all the cards available 
+            
+        Option #2 [Idea is less clicks on this]
+            ==> initial click will then bring us to a page all of the cards available
+            ==> we will have the abiltiy to search and filter 
+            ==> just need a good way of displaying the cards now... 
+    */ 
+
     return ( 
         <div> 
             <p> Hello World </p>
